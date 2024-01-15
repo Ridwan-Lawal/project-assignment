bbc8bd9ec7e1650088bf443026e6803257ae99c8
Ridwan-Lawal <lawal6382@gmail.com>
2024-01-15 15:25:45 +0100

    built the nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import { IoCartOutline, IoPersonOutline } from "react-icons/io5";
+
+export function NavBar({ cartNumber = 0 }) {
+  return (
+    <nav className="border py-4 px-5 flex items-center justify-between">
+      {/* logo */}
+      <section>
+        <img src="/logo.svg" alt="" className="w-11" />
+      </section>
+
+      {/* login and cart */}
+
+      <section className="border gap-10 flex items-center">
+        <div>
+          <IoPersonOutline className="text-[27px] cursor-pointer" />
+        </div>
+        <div className="relative">
+          <IoCartOutline className="text-[33px] cursor-pointer" />
+          <p className="bg-red-600 font-medium text-white flex justify-center absolute px-[5px] text-sm items-center rounded-full -bottom-1 right-0">
+            {cartNumber}
+          </p>
+        </div>
+      </section>
+    </nav>
+  );
+}
