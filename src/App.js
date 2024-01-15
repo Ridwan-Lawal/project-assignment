7ce8d6e0b7a0ea333eef6e633894bffb2ab8bb79
Ridwan-Lawal <lawal6382@gmail.com>
2024-01-15 15:24:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { IoCartOutline, IoPersonOutline } from "react-icons/io5";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-poppins">
+      <NavBar />
     </div>
   );
 }
 
-export default App;
+function NavBar({ cartNumber = 0 }) {
+  return (
+    <nav className="border py-4 px-5 flex items-center justify-between">
+      {/* logo */}
+      <section>
+        <img src="/logo.svg" alt="" className="w-11" />
+      </section>
+
+      {/* login and cart */}
+
+      <section className="border gap-10 flex items-center">
+        <div>
+          <IoPersonOutline className="text-[27px] cursor-pointer" />
+        </div>
+        <div className="relative">
+          <IoCartOutline className="text-[33px] cursor-pointer" />
+          <p className="bg-red-600 font-medium text-white flex justify-center absolute px-[5px] text-sm items-center rounded-full -bottom-1 right-0">
+            {cartNumber}
+          </p>
+        </div>
+      </section>
+    </nav>
+  );
+}
